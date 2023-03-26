431a488bc43716170719248beac89c5e98a7bca2
ZitelliDZ <zitelli.dz@gmail.com>
2023-03-26 17:48:16 -0300

    Arreglo id Select Peso Argentino

--- src/data/monedas.js
+++ src/data/monedas.js
@@ -3,7 +3,7 @@ const monedas = [
     {id: 'MXN', nombre:'Peso Mexicano'},
     {id: 'EUR', nombre:'Euro'},
     {id: 'GBP', nombre:'Libra Esterlina'},
-    {id: 'ARG', nombre:'Peso Argentino'}
+    {id: 'ARS', nombre:'Peso Argentino'}
 ]
 
 export {
